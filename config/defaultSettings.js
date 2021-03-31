52e52676ee03ad2868f36e824055c9b601aaf4fe
yangt57 <yangt57@chinaunicom.cn>
2021-03-31 09:18:21 +0800

    Initial

---
+++ config/defaultSettings.js
@@ -0,0 +1,30 @@
+const proSettings = {
+  "navTheme": "light",
+  "primaryColor": "#1890ff",
+  "layout": "mix",
+  "contentWidth": "Fluid",
+  "fixedHeader": true,
+  "fixSiderbar": true,
+  "title": "智慧城市开放平台",
+  "pwa": false,
+  "iconfontUrl": "",
+  "menu": {
+    "locale": true
+  },
+  "headerHeight": 48,
+  "splitMenus": true
+};
+// {
+//   navTheme: 'dark',
+//   // 拂晓蓝
+//   primaryColor: '#1890ff',
+//   layout: 'side',
+//   contentWidth: 'Fluid',
+//   fixedHeader: false,
+//   fixSiderbar: true,
+//   colorWeak: false,
+//   title: '智慧城市开放平台',
+//   pwa: false,
+//   iconfontUrl: '',
+// };
+export default proSettings;
